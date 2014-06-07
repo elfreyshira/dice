7b73d4debac8546eacbea8060a47a073e08e258f
Elfrey Shira <elf.shira@gmail.com>
2014-06-06 22:13:56 -0400

    FINALLY A GOOD ALGORITHM!

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,141 @@
 var app = angular.module('dice', ['angular.directives-round-progress']);
 
+app.factory('pickNumberFromDistribution', function() {
+
+	var alwaysTrue = _.constant(true);
+
+	/*
+	Takes in a distribution and randomly picks a value from it
+	- distribution {Object} mapping of number to probability distribution
+	- meetCriteria {Function} takes distribution value and returns boolean
+	*/
+	return function pickNumberFromDistribution(distribution, meetCriteria) {
+
+		meetCriteria = meetCriteria || alwaysTrue;
+
+		var sumTotal = _.reduce(distribution, function(sum, number) {
+			return sum + number;
+		}, 0);
+
+		var possibleValues = _.keys(distribution);
+
+		var rawRoll = Math.random() * sumTotal;
+		var currentSum = 0;
+
+		for (var i = 0; i < possibleValues.length; i++) {
+			var number = possibleValues[i];
+			var distributionValue = distribution[number];
+			currentSum += distributionValue;
+			if (currentSum > rawRoll) {
+				if (meetCriteria(distributionValue)) {
+					return number;
+				}
+				else {
+					return pickNumberFromDistribution(distribution, meetCriteria);
+				}
+			}
+		}
+	};
+});
+
+app.service('equalChanceDistributionBias', function($window, pickNumberFromDistribution) {
+
+	var startArr = [];
+	for (var i = 1; i <= 6; i++ ) {
+		for (var j = 1; j <= 6; j++) {
+			startArr.push(i + j);
+		}
+	}
+	var possibleValues = _.unique(startArr);
+	var originalMappedValues = _.countBy(startArr);
+
+	// 36
+	var sumTotal = startArr.length;
+
+	/*
+		{
+			2: 1/36,
+			...
+			7: 6/26,
+			...
+		}
+	*/
+	var originalProbabilities = _.mapValues(originalMappedValues, function(number) {
+		return number / sumTotal;
+	});
+
+	var originalDistributionAverage = averageValueChosen(originalMappedValues);
+	
+	var currentMappedValues = _.mapValues(originalMappedValues, function(distributionValue) {
+		return distributionValue * originalDistributionAverage;
+	});
+
+	/*
+	After each roll, the probability is taken down to 0 for that number.
+	Given a mapping, this returns the average probability that will be distributed.
+
+	Around 4.0556 for originalMappedValues
+	*/
+	function averageValueChosen(mapping) {
+		return _.reduce(mapping, function(sum, probability, number) {
+			return sum + (probability * originalProbabilities[number]);
+		}, 0);
+	}
+
+	function shiftValues(numberRolled, valueToRedistribute) {
+
+		currentMappedValues[numberRolled] -= valueToRedistribute;
+		_.forEach(possibleValues, function(number) {
+			currentMappedValues[number] += valueToRedistribute * originalProbabilities[number];
+		});
+
+	}
+
+	function roll() {
+		var valueToRedistribute = averageValueChosen(currentMappedValues);
+		var numberRolled = pickNumberFromDistribution(currentMappedValues, function(distributionValue) {
+			return distributionValue >= valueToRedistribute
+		});
+
+		shiftValues(numberRolled, valueToRedistribute);
+
+		rollHistory[numberRolled] += 1;
+
+		return numberRolled;
+	}
+
+	//////////////////////////////
+	//////////////////////////////
+	// rollHistory = _.reduce(possibleValues, function(result, num) {
+	// 		result[num] = 0;
+	// 		return result;
+	// 	}, {});
+
+	// $window.showState = function() {
+	// 	console.log('rollHistory');
+	// 	console.log(rollHistory);
+
+	// 	console.log('currentMappedValues');
+	// 	console.log(currentMappedValues);
+
+	// 	// console.log('rollHistory');
+	// 	// console.log(rollHistory);
+	// };
+
+	// $window.roll = function() {
+	// 	roll();
+	// }
+
+	////////////////////////////
+	////////////////////////////
+
+	return {
+		roll: roll
+	};
+
+
+});
+
 app.service('finiteListBias', function() {
 	var startArr = [];
 	for (var i = 1; i <= 6; i++ ) {
@@ -127,10 +263,11 @@ app.service('equalSpreadBias', function($window) {
 
 });
 
-app.controller('DiceController', function($scope, $timeout, finiteListBias, equalSpreadBias) {
+app.controller('DiceController', function($scope, $timeout, equalChanceDistributionBias) {
 
 	// var roller = finiteListBias;
-	var roller = equalSpreadBias;
+	// var roller = equalSpreadBias;
+	var roller = equalChanceDistributionBias;
 
 	$scope.roundProgressData = {
 		label: "Hold",
