d378713a2be0fff029306b53dd3e2f507bea7e5e
Elfrey Shira <elf.shira@gmail.com>
2014-06-08 13:42:41 -0400

    clean up algorithm. clean up code.

--- scripts/app.js
+++ scripts/app.js
@@ -69,11 +69,7 @@ app.service('equalChanceDistributionBias', function($window, pickNumberFromDistr
         return number / sumTotal;
     });
 
-    var originalDistributionAverage = averageValueChosen(originalMappedValues);
-    
-    var currentMappedValues = _.mapValues(originalMappedValues, function(distributionValue) {
-        return distributionValue * originalDistributionAverage;
-    });
+    var currentMappedValues = _.cloneDeep(originalMappedValues);
 
     /*
     After each roll, the probability is taken down to 0 for that number.
@@ -98,9 +94,7 @@ app.service('equalChanceDistributionBias', function($window, pickNumberFromDistr
 
     function roll() {
         var valueToRedistribute = averageValueChosen(currentMappedValues);
-        var numberRolled = pickNumberFromDistribution(currentMappedValues, function(distributionValue) {
-            return distributionValue >= valueToRedistribute
-        });
+        var numberRolled = pickNumberFromDistribution(currentMappedValues);
 
         shiftValues(numberRolled, valueToRedistribute);
 
@@ -110,26 +104,26 @@ app.service('equalChanceDistributionBias', function($window, pickNumberFromDistr
     }
 
     //////////////////////////////
+    // Debugging purposes
     //////////////////////////////
-    // rollHistory = _.reduce(possibleValues, function(result, num) {
-    //      result[num] = 0;
-    //      return result;
-    //  }, {});
+   //  rollHistory = _.reduce(possibleValues, function(result, num) {
+   //      result[num] = 0;
+   //      return result;
+   //  }, {});
 
-    // $window.showState = function() {
-    //  console.log('rollHistory');
-    //  console.log(rollHistory);
+   //  $window.showState = function() {
+   //      console.log('rollHistory');
+   //      console.log(rollHistory);
 
-    //  console.log('currentMappedValues');
-    //  console.log(currentMappedValues);
+   //      console.log(averageValueChosen(currentMappedValues));
 
-    //  // console.log('rollHistory');
-    //  // console.log(rollHistory);
-    // };
+   //      console.log('currentMappedValues');
+   //      console.log(currentMappedValues);
+   //  };
 
-    // $window.roll = function() {
-    //  roll();
-    // }
+   //  $window.roll = function() {
+   //     roll();
+   // }
 
     ////////////////////////////
     ////////////////////////////
@@ -137,134 +131,6 @@ app.service('equalChanceDistributionBias', function($window, pickNumberFromDistr
     return {
         roll: roll
     };
-
-
-});
-
-app.service('finiteListBias', function() {
-    var startArr = [];
-    for (var i = 1; i <= 6; i++ ) {
-        for (var j = 1; j <= 6; j++) {
-            startArr.push(i + j);
-        }
-    }
-    var startLength = startArr.length;
-
-    var currentArr = startArr.slice();
-
-    function roll() {
-
-        if (currentArr.length < startLength/2) {
-            currentArr = currentArr.concat(startArr);
-        }
-
-        var index = Math.floor(Math.random()*currentArr.length);
-        var temp = currentArr[0];
-        currentArr[0] = currentArr[index];
-        currentArr[index] = temp;
-
-        return currentArr.shift();
-    }
-
-    return {
-        roll: roll
-    };
-
-});
-
-app.service('equalSpreadBias', function($window) {
-    var startArr,
-    total,
-    currentArr,
-    possibleValues,
-    originalMappedValues,
-    currentMappedValues,
-    rollHistory;
-
-    function reset() {
-        startArr = [];
-        for (var i = 1; i <= 6; i++ ) {
-            for (var j = 1; j <= 6; j++) {
-                startArr.push(i + j);
-            }
-        }
-        total = startArr.length;
-        currentArr = startArr.slice();
-        possibleValues = _.unique(startArr);
-        originalMappedValues = _.countBy(startArr);
-        currentMappedValues = _.countBy(currentArr);
-        rollHistory = _.reduce(
-            possibleValues,
-            function(result, num) {
-                result[num] = 0;
-                return result;
-            },
-            {}
-            );
-    }
-
-    reset();
-
-    function shiftValues(numberRolled) {
-        currentMappedValues[numberRolled] -= 1;
-        possibleValues.forEach(function(val) {
-            if (val !== numberRolled) {
-                var addedValue = originalMappedValues[val] / 36.0;
-                var extraValueFromRolledProportion = addedValue
-                * originalMappedValues[numberRolled] / (36.0 - originalMappedValues[numberRolled]);
-                currentMappedValues[val] += (addedValue + extraValueFromRolledProportion);
-            }
-        });
-    }
-
-    function rollSlave() {
-        var rawRoll = Math.random() * total;
-        var currentSum = 0;
-
-        var rolled;
-        for (var i = 0; i < possibleValues.length; i++) {
-            var value = possibleValues[i];
-            if (currentSum + currentMappedValues[value] > rawRoll) {
-                return value;
-            }
-            currentSum += currentMappedValues[value];
-        }
-    }
-
-    function rollHelper() {
-        var numberRolled = rollSlave();
-        if (currentMappedValues[numberRolled] >= originalMappedValues[numberRolled]) {
-            shiftValues(numberRolled);
-            return numberRolled;
-        }
-        else {
-            return rollHelper();
-        }
-    }
-
-    function roll() {
-        var numberRolled = rollHelper();
-        rollHistory[numberRolled] += 1;
-        return numberRolled;
-    }
-
-    (function() {
-        // comment out when debugging
-        // return;
-        $window.showState = function() {
-            console.log(currentMappedValues);
-            console.log(rollHistory);
-        };
-        $window.reset = reset;
-        $window.roll = function() {
-            console.log(roll());
-            $window.showState();
-        };
-    })();
-
-    return {
-        roll: roll
-    }
 
 });
 
@@ -285,7 +151,6 @@ app.factory('getRollAudio', function() {
     return function() {
         return _.sample(sounds);
     };
-
 
 });
 
